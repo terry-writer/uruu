b762b65c2dd9c6aa2524918a0ac405695a4f91af
Hiroto-Oohashi <byebye0713@gmail.com>
2020-12-26 13:11:58 +0900

    2回目（ガード節を利用した関数）

--- function.php
+++ function.php
@@ -1,34 +1,14 @@
 <?php
 
-
 function judge($number){
-    if($number % 4 ===0){
-        if($number % 100 !== 0){
-            echo 'うるう年です';
-        }else{
-            if($number % 400 ===0){
-                echo 'うるう年です';
-            }
-        }
-    }else{
-        echo 'うるう年ではありません';
+    if($number % 4 !== 0){
         return false;
     }
+    if($number % 100 === 0){
+        return false;
+    }
+    if($number % 400 === 0){
+        return true;
+    }
+        return true;
 }
-
-
-echo "2003年は"; 
-judge(2003);
-
-
-echo '2004年は';
-judge(2004);
-
-echo '2017年は';
-judge(2017);
-
-echo '2020年は'; 
-judge(2020);
-
-echo '2022年は';
-judge(2022);
