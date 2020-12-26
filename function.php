612dc1a3003a68257a8d71c11e9f511af76ac0c9
Hiroto-Oohashi <byebye0713@gmail.com>
2020-12-26 13:14:37 +0900

    コメント追記

--- function.php
+++ function.php
@@ -2,13 +2,17 @@
 
 function judge($number){
     if($number % 4 !== 0){
+        //4で割り切れない場合は、うるう年ではない
         return false;
     }
     if($number % 100 === 0){
+        //4で割り切れ、100で割り切れるものはうるう年ではない
         return false;
     }
     if($number % 400 === 0){
+        //4で割り切れ、100で割り切れず、なおかつ400で割り切れるものはうるうどしである
         return true;
     }
+        //4で割り切れるモノはうるう年である
         return true;
 }
