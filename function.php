9d4444b79fb4fc5be08373705331c99432c47ebd
Hiroto-Oohashi <byebye0713@gmail.com>
2020-12-26 13:24:59 +0900

    変数名変更

--- function.php
+++ function.php
@@ -1,15 +1,15 @@
 <?php
 
-function judge($number){
-    if($number % 4 !== 0){
+function judge($check_number){
+    if($check_number % 4 !== 0){
         //4で割り切れない場合は、うるう年ではない
         return false;
     }
-    if($number % 100 === 0){
+    if($check_number % 100 === 0){
         //4で割り切れ、100で割り切れるものはうるう年ではない
         return false;
     }
-    if($number % 400 === 0){
+    if($check_number % 400 === 0){
         //4で割り切れ、100で割り切れず、なおかつ400で割り切れるものはうるうどしである
         return true;
     }
